efa712dc5cbd50877514c856440643614a0a66e8
timartin <timartin@Inception.myguest.virtualbox.org>
2024-10-10 19:41:07 +0100

    plppls

--- srcs/requirements/mariadb/conf/init_script.sh
+++ srcs/requirements/mariadb/conf/init_script.sh
@@ -4,13 +4,15 @@ service mariadb start
 
 sleep 5
 
-mariadb -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
+mysql -u root -p$MYSQL_ROOT_PASSWORD -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
 
-mariadb -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mysql -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
 
-mariadb -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO \`${MYSQL_USER}\`@'%';"
+mysql -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
 
-mariadb -e "FLUSH PRIVILEGES;"
+mysql -u root -p$MYSQL_ROOT_PASSWORD  -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO \`${MYSQL_USER}\`@'%';"
+
+mysql -u root -p$MYSQL_ROOT_PASSWORD  -e "FLUSH PRIVILEGES;"
 
 mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
 
